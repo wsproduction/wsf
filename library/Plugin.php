fd456a00b443a5d100a59d1b3c23985c52da85df
kit <warmansuganda@yahoo.com>
2012-12-15 09:43:00 +0700

    custom

--- library/Plugin.php
+++ library/Plugin.php
@@ -53,134 +53,80 @@ class Plugin {
     /* {LIST PLUGIN - Javascript} */
 
     public function jQuery() {
-        //array_push(Src::$plugin, $this->path_src . 'js/jquery-1.7.1.js');
-        Src::$plugin[0] = $this->path_src . 'js/jquery-1.7.1.js';
+        Src::$plugin[0] = $this->path_src . 'js/jquery-1.8.3.min.js';
     }
     
     public function jQueryUI() {
-        //array_push(Src::$plugin, $this->path_src . 'js/jquery-ui/jquery-ui-1.8.13.custom.min.js');
-        Src::$plugin[1] = $this->path_src . 'js/jquery-ui/jquery-ui-1.9.1.custom.js';
-        array_push(Src::$css, $this->path_src . 'js/jquery-ui/css/smoothness/jquery-ui-1.8.13.custom.css');
-    }
-
-    public function jQueryJson() {
-        //array_push(Src::$plugin, $this->path_src . 'js/jquery.json-2.3.min.js');
-        Src::$plugin[2] = $this->path_src . 'js/jquery.json-2.3.min.js';
+        Src::$plugin[1] = $this->path_src . 'js/jquery-ui-1.9.2.custom/js/jquery-ui-1.9.2.custom.min.js';
+        array_push(Src::$css, $this->path_src . 'js/jquery-ui-1.9.2.custom/css/smoothness/jquery-ui-1.9.2.custom.min.css');
     }
 
     public function jQueryForm() {
-        //array_push(Src::$plugin, $this->path_src . 'js/jquery.form.js');
-        Src::$plugin[3] = $this->path_src . 'js/jquery.form.js';
+        Src::$plugin[2] = $this->path_src . 'js/jquery.form.js';
     }
 
     public function jQueryAutoNumeric() {
-        //array_push(Src::$plugin, $this->path_src . 'js/jquery.auto.numeric.js');
-        Src::$plugin[4] = $this->path_src . 'js/jquery.auto.numeric.js';
+        Src::$plugin[3] = $this->path_src . 'js/jquery.auto.numeric.js';
     }
     
     public function jQueryCookie() {
-        //array_push(Src::$plugin, $this->path_src . 'js/jquery.cookie.js');
-        Src::$plugin[5] = $this->path_src . 'js/jquery.cookie.js';
+        Src::$plugin[4] = $this->path_src . 'js/jquery.cookie.js';
     }
 
     public function jQueryValidation() {
-        //array_push(Src::$plugin, $this->path_src . 'js/jquery.validate.js');
-        Src::$plugin[6] = $this->path_src . 'js/jquery.validate.js';
-        Src::$plugin[26] = $this->path_src . 'js/jquery.validate.custom.js';
+        Src::$plugin[5] = $this->path_src . 'js/jquery.validate.js';
+        Src::$plugin[6] = $this->path_src . 'js/jquery.validate.custom.js';
     }
 
     public function jQueryAlphaNumeric() {
-        //array_push(Src::$plugin, $this->path_src . 'js/jquery.alphanumeric.pack.js');
         Src::$plugin[7] = $this->path_src . 'js/jquery.alphanumeric.pack.js';
     }
 
     public function jQueryBase64() {
-        //array_push(Src::$plugin, $this->path_src . 'js/jquery.base/base/jquery.base.min.js');
         Src::$plugin[8] = $this->path_src . 'js/jquery.base/base/jquery.base.min.js';
     }
 
     public function poshytip() {
-        //array_push(Src::$plugin, $this->path_src . 'js/poshytip/jquery.poshytip.js');
-        //array_push(Src::$plugin, $this->path_src . 'js/poshytip/config.poshytip.js');
         Src::$plugin[9] = $this->path_src . 'js/poshytip/jquery.poshytip.js';
         Src::$plugin[10] = $this->path_src . 'js/poshytip/config.poshytip.js';
         array_push(Src::$css, $this->path_src . 'js/poshytip/tip-twitter/tip-twitter.css');
     }
 
     public function passwordMeter() {
-        //array_push(Src::$plugin, $this->path_src . 'js/passwordmeter/jquery.pwdMeter.js');
         Src::$plugin[11] = $this->path_src . 'js/passwordmeter/jquery.pwdMeter.js';
         array_push(Src::$css, $this->path_src . 'js/passwordmeter/style.css');
     }
 
-    public function flexDropDown() {
-        //array_push(Src::$plugin, $this->path_src . 'js/flexdropdown/flexdropdown.js');
-        Src::$plugin[12] = $this->path_src . 'js/flexdropdown/flexdropdown.js';
-        array_push(Src::$css, $this->path_src . 'js/flexdropdown/flexdropdown.css');
-    }
-
-    public function cleditor() {
-        //array_push(Src::$plugin, $this->path_src . 'js/cleditor130/jquery.cleditor.js');
-        Src::$plugin[13] = $this->path_src . 'js/cleditor130/jquery.cleditor.js';
-        array_push(Src::$css, $this->path_src . 'js/cleditor130/jquery.cleditor.css');
-    }
-
     public function elrte() {
-        //array_push(Src::$plugin, $this->path_src . 'js/elrte13/js/elrte.min.js');
-        //array_push(Src::$plugin, $this->path_src . 'js/elrte13/js/i18n/elrte.ru.js');
-        Src::$plugin[14] = $this->path_src . 'js/elrte13/js/elrte.min.js';
-        Src::$plugin[15] = $this->path_src . 'js/elrte13/js/i18n/elrte.ru.js';
+        Src::$plugin[12] = $this->path_src . 'js/elrte13/js/elrte.min.js';
+        Src::$plugin[13] = $this->path_src . 'js/elrte13/js/i18n/elrte.ru.js';
         array_push(Src::$css, $this->path_src . 'js/elrte13/css/elrte.min.css');
     }
 
-    public function jDialogBox() {
-        //array_push(Src::$plugin, $this->path_src . 'js/dialogbox/js/jquery.reveal.js');
-        Src::$plugin[16] = $this->path_src . 'js/dialogbox/js/jquery.reveal.js';
-        array_push(Src::$css, $this->path_src . 'js/dialogbox/css/styles.css');
-    }
-
     public function tokenInput() {
-        //array_push(Src::$plugin, $this->path_src . 'js/tokeninput/src/jquery.tokeninput.js');
-        Src::$plugin[17] = $this->path_src . 'js/tokeninput/src/jquery.tokeninput.js';
+        Src::$plugin[14] = $this->path_src . 'js/tokeninput/src/jquery.tokeninput.js';
         array_push(Src::$css, $this->path_src . 'js/tokeninput/styles/token-input-facebook.css');
     }
 
     public function highChart() {
-        //array_push(Src::$plugin, $this->path_src . 'js/highchart/highcharts.js');
-        //array_push(Src::$plugin, $this->path_src . 'js/highchart/modules/exporting.js');
-        Src::$plugin[18] = $this->path_src . 'js/highchart/highcharts.js';
-        Src::$plugin[19] = $this->path_src . 'js/highchart/modules/exporting.js';
+        Src::$plugin[15] = $this->path_src . 'js/highchart/highcharts.js';
+        Src::$plugin[16] = $this->path_src . 'js/highchart/modules/exporting.js';
     }
 
     public function flexiGrid() {
-        //array_push(Src::$plugin, $this->path_src . 'js/flexigrid/js/flexigrid.pack.js');
-        Src::$plugin[20] = $this->path_src . 'js/flexigrid/js/flexigrid.pack.js';
+        Src::$plugin[17] = $this->path_src . 'js/flexigrid/js/flexigrid.pack.js';
         array_push(Src::$css, $this->path_src . 'js/flexigrid/css/flexigrid.pack.css');
     }
 
     public function customScrollbar() {
-        //array_push(Src::$plugin, $this->path_src . 'js/custom-scrollbar/jquery.mousewheel.min.js');
-        //array_push(Src::$plugin, $this->path_src . 'js/custom-scrollbar/jquery.mCustomScrollbar.js');
-        Src::$plugin[21] = $this->path_src . 'js/custom-scrollbar/jquery.mousewheel.min.js';
-        Src::$plugin[22] = $this->path_src . 'js/custom-scrollbar/jquery.mCustomScrollbar.js';
+        Src::$plugin[18] = $this->path_src . 'js/custom-scrollbar/jquery.mousewheel.min.js';
+        Src::$plugin[19] = $this->path_src . 'js/custom-scrollbar/jquery.mCustomScrollbar.js';
         array_push(Src::$css, $this->path_src . 'js/custom-scrollbar/jquery.mCustomScrollbar.css');
     }
 
     public function jQueryAddress() {
-        //array_push(Src::$plugin, $this->path_src . 'js/jquery.address-1.4.min.js');
-        Src::$plugin[23] = $this->path_src . 'js/jquery.address-1.4.min.js';
+        Src::$plugin[20] = $this->path_src . 'js/jquery.address-1.4.min.js';
     }
     
-    public function jQuerySelectBox() {
-        //array_push(Src::$plugin, $this->path_src . 'js/jquery.address-1.4.min.js');
-        Src::$plugin[24] = $this->path_src . 'js/jquery.selectbox-0.2/js/jquery.selectbox-0.2.js';
-        array_push(Src::$css, $this->path_src . 'js/jquery.selectbox-0.2/css/jquery.selectbox.css');
-    }
-    
-    public function rLoader() {
-        //array_push(Src::$plugin, $this->path_src . 'js/jquery.requireScript-1.2.1');
-        Src::$plugin[25] = $this->path_src . 'js/rloader/rloader1.5.4_min.js';
-    }
-
 }
 
--- plugin/js/jquery.validate.custom.js
+++ plugin/js/jquery.validate.custom.js
@@ -3,23 +3,4 @@
  * and open the template in the editor.
  */
 
-
-jQuery.validator.addMethod("largerDateFrom", function(value, element, param) {
-    var temp1 = $(param).val(); 
-    var temp2 = value;
-    
-    var val1 = new Date(temp1);
-    var val2 = new Date(temp2);
-    
-    return this.optional(element) || val2 >= val1;
-}, "Incorect date.");
-
-jQuery.validator.addMethod("smallerDateFrom", function(value, element, param) {
-    var temp1 = $(param).val(); 
-    var temp2 = value;
-    
-    var val1 = new Date(temp1);
-    var val2 = new Date(temp2);
-    
-    return this.optional(element) || val2 >= val1;
-}, "Incorect date.");
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('f.b.c("k",e(2,3,4){0 5=$(4).a();0 1=2;0 6=7 8(5);0 9=7 8(1);g h.i(3)||9>=6},"j d.");f.b.c("l",e(2,3,4){0 5=$(4).a();0 1=2;0 6=7 8(5);0 9=7 8(1);g h.i(3)||9>=6},"j d.");',22,22,'var|temp2|value|element|param|temp1|val1|new|Date|val2|val|validator|addMethod|date|function|jQuery|return|this|optional|Incorect|largerDateFrom|smallerDateFrom'.split('|'),0,{}));
